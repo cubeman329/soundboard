24a7a3e2cd74dcbc841044c018724c924e78f0c7
cubeman329 <cubeman329@gmail.com>
2024-09-10 22:59:13 -0500

    added 2 sounds

--- alt.js
+++ alt.js
@@ -1,7 +1,7 @@
 const audioFiles = [
     'boom.mp3', 'emotionaldamage.mp3', 'bird.mp3', 'fart.mp3', 'bababooey.mp3',
     'notif.mp3', 'bruh.mp3', 'heheheha.mp3', 'MUSIC MAN.mp3', 'bonk.mp3',
-    'fart3.mp3', 'hwg.mp3', 'oh mah gahd.mp3', 'bup.mp3', 'getout.mp3', 'boing.mp3'
+    'fart3.mp3', 'hwg.mp3', 'omg.mp3', 'bup.mp3', 'getout.mp3', 'boing.mp3', 'huh.mp3'
 ];
 
 const audioMap = new Map();
@@ -13,14 +13,18 @@ audioFiles.forEach(src => {
     audioMap.set(id, audio);  // Store audio elements in the map
     console.log(src + ' has loaded successfully');
     
-    if (['fart.mp3', 'bababooey.mp3', 'bonk.mp3', 'fart3.mp3', 'oh mah gahd.mp3', 'bup.mp3', 'getout.mp3', 'boing.mp3'].includes(src)) {
+    if (['fart.mp3', 'bababooey.mp3', 'bonk.mp3', 'fart3.mp3', 'omg.mp3', 'bup.mp3', 'getout.mp3', 'boing.mp3', 'huh.mp3'].includes(src)) {
         audio.className = 'audio';
     }
 });
 
+const volumeSlider = document.getElementById('volumeslider');
+
 function playAudioById(id) {
     const audioElement = audioMap.get(id);
     if (audioElement) {
+        audioElement.currentTime = 0.15;
+        audioElement.volume = volumeSlider.value
         audioElement.play();
     } else {
         console.log(`Audio with ID ${id} not found`);
@@ -28,15 +32,6 @@ function playAudioById(id) {
 }
 
 // Play the 'fart' sound
-playAudioById('fart');
+// playAudioById('fart');
 
-const volumeSlider = document.getElementById('volumeslider');
 
-function setVolume() {
-    const volume = volumeSlider.value;
-    audioElements.forEach(audio => {
-        audio.volume = volume;
-    });
-}
-
-volumeSlider.addEventListener('input', setVolume);
